3140edcd0e2dbe259c7367a1cc098f0f96a0c2bb
TreeLand <cowseer60906@gmail.com>
2024-07-22 23:29:52 +0800

    Feat: Revise run_clear.sh

--- run_clear.sh
+++ run_clear.sh
@@ -1,18 +1,14 @@
 #!/bin/sh
 
-# 定义要删除的目录
 dirs="./log ./model ./predictions ./summary"
 
-# 打印警告信息
 echo "WARNING: You are about to delete the following directories:"
 for dir in $dirs; do
     echo " - $dir"
 done
 
-# 提示用户确认
 read -p "Do you want to continue? (yes/no): " confirmation
 
-# 根据用户输入决定是否删除
 if [ "$confirmation" = "yes" ]; then
     for dir in $dirs; do
         if [ -d "$dir" ]; then
